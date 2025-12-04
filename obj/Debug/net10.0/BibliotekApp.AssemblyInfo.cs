ab0cd3f9eab9a9a0ab272ef355039904691f657e
A.AlKafri <ahmed_kf83@hotmail.com>
2025-12-05 00:40:23 +0100

    Final commit - upload BibliotekApp project2

--- obj/Debug/net10.0/BibliotekApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/BibliotekApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotekApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc11820ffbf7afe7c4abdd16145e9dde2643aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotekApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotekApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
